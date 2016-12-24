4bf6e517e7ab8561b3c3d4fc34b6bea232bd617c
Yoshihiko Kume <yoshipiko@gmail.com>
2016-12-24 07:32:15 +0000

    新規登録

--- book_shelf_v2/app/controllers/books_controller.rb
+++ book_shelf_v2/app/controllers/books_controller.rb
@@ -5,4 +5,21 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+  
+  ### ここからはprivate!!! ###
+  private
+  def book_params
+    params.require(:book).permit(:name, :price, :publish, :publish_date)
+  end
+  
 end
